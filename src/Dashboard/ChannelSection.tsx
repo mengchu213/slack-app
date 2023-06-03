be318212f7f7e47159518aa55ad46af78244c70f
Jose Infante <joseinfa1022@gmail.com>
2023-06-03 11:47:04 +0800

    channels

--- src/Dashboard/ChannelSection.tsx
+++ src/Dashboard/ChannelSection.tsx
@@ -13,7 +13,7 @@ const ChannelSection: React.FC<ChannelSectionProps> = ({
   return (
     <div>
       <div className="px-5 py-3 flex justify-between items-center">
-        <h2 className="font-semibold">Channels</h2>
+        <h2 className="font-semibold">Channels.</h2>
         <button
           className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 "
           onClick={onAddChannel}
