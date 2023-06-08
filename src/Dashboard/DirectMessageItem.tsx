7d78604ef82c54eb9e3af115ebbac3ef456d6113
bamirade <118993260+bamirade@users.noreply.github.com>
2023-06-08 15:47:48 +0800

    Button UX?

--- src/Dashboard/DirectMessageItem.tsx
+++ src/Dashboard/DirectMessageItem.tsx
@@ -44,14 +44,31 @@ export const DirectMessageItems = () => {
         <div key={index}>
           {list.map((user: any) => (
             <button
-              key={user.id}
-              onClick={() => handleButtonClick(user.id)}
-              className={`px-5 py-1 hover:bg-gray-600 cursor-pointer ${
-                user.id === selectedId ? "bg-gray-600 text-white" : ""
-              }`}
-            >
-              {user.uid}
-            </button>
+            key={user.id}
+            onClick={() => handleButtonClick(user.id)}
+            className={`flex items-center justify-between px-5 py-2 rounded-lg hover:bg-gray-600 cursor-pointer transition duration-200 w-11/12 text-left ${
+              user.id === selectedId ? "bg-gray-600 text-white shadow-lg" : ""
+            }`}
+          >
+            <span className="font-bold">{user.uid}</span>
+            {user.id === selectedId && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 ml-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            )}
+          </button>
+          
           ))}
         </div>
       ))}
