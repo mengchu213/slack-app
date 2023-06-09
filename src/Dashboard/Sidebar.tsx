b62abf00ab50dd9cc73aed0f30df8277c377608a
bamirade <118993260+bamirade@users.noreply.github.com>
2023-06-09 20:41:49 +0800

    UX Update

--- src/Dashboard/Sidebar.tsx
+++ src/Dashboard/Sidebar.tsx
@@ -8,7 +8,7 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ onAddChannel, onAddUser }) => {
   return (
-    <div className="w-64 bg-gray-800 text-white overflow-auto h-[94.7vh] scrollbar">
+    <div className="w-64 bg-gray-800 text-white overflow-auto h-[94.9vh] scrollbar">
       <div className="px-5 py-5">
         <h1 className="text-2xl font-semibold">Avion School</h1>
       </div>
